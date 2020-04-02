b52e37e3b923f04cbd9a90333763504802bb1e95
TheInfinity007 <ramchandra71999@gmail.com>
2020-04-02 21:16:26 +0530

    add the keyboard events functionality

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-script
+var html = document.querySelector("html");
+html.addEventListener("keydown", function(event){
+	console.log(event.key);
+	var text = document.calc.txt.value;
+	if(parseInt(event.key)){
+		text += event.key;
+		document.calc.txt.value = text;
+	}else if(event.key == '+' || event.key == "-" || event.key == "*" || event.key == "/"){
+		text += event.key;
+		document.calc.txt.value = text;
+	}else if(event.key == "Enter"){
+		text = eval(text);
+		document.calc.txt.value = eval(text);
+	}else if(event.key == "Backspace"){
+		text = text.slice(0, text.length-1);
+		document.calc.txt.value = text;
+	}else if(event.key == "(" || event.key == ")")
+	{
+		text += event.key;
+		document.calc.txt.value = text;
+	}
+});
